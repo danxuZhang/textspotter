24acfba2b436b29cbaaf3b86c3b47b7dec0de4f4
Danxu Zhang <dxzhang49@gmail.com>
2023-11-08 16:38:46 +0800

    chnage to ctypes

--- include/textspotter/textspotter.hpp
+++ include/textspotter/textspotter.hpp
@@ -1,13 +1,35 @@
 #pragma once
 
+#include <tesseract/baseapi.h>
+#include <opencv2/opencv.hpp>
 #include <string>
 
-namespace textspotter {
-    class TextSpotter {
-    public:
-        TextSpotter() = default;
-        ~TextSpotter() = default;
+class TesseractApi {
+ public:
+  TesseractApi(const char *language = "eng");
+  TesseractApi(std::string_view language);
+  ~TesseractApi();
 
-        auto Hello() -> std::string;
-    };
+  std::unique_ptr<tesseract::TessBaseAPI> api_;
+};
+
+auto LoadImage(const char *image_path) -> cv::Mat;
+auto LoadImage(std::string_view image_path) -> cv::Mat;
+
+auto Preprocess(const cv::Mat &image) noexcept -> cv::Mat;
+
+auto RecognizeText(const cv::Mat &image, float conf_threshold) noexcept
+    -> std::tuple<std::vector<std::string>, std::vector<cv::Rect>, std::vector<float>>;
+
+auto MatchText(const cv::Mat &image, std::string_view target) noexcept -> cv::Point;
+auto MatchText(const cv::Mat &image, const char *target) noexcept -> cv::Point;
+auto MatchText(const char *image_path, const char *target) noexcept -> cv::Point;
+
+auto CalcLevenshteinDistance(std::string_view s1, std::string_view s2) noexcept -> int;
+
+auto GetRectCenter(const cv::Rect &rect) noexcept -> cv::Point;
+
+extern "C" {
+_declspec(dllexport) bool match_text_from_file(const char *image_path, const char *target, int *x, int *y);
+_declspec(dllexport) bool match_text(const cv::Mat image, const char *target, int *x, int *y);
 }
--- src/textspotter.cpp
+++ src/textspotter.cpp
@@ -1,7 +1,162 @@
 #include "textspotter/textspotter.hpp"
+#include <cctype>
 
-namespace textspotter {
-    auto TextSpotter::Hello() -> std::string {
-        return "Hello, World!";
+TesseractApi::TesseractApi(const char *language) : api_(std::make_unique<tesseract::TessBaseAPI>()) {
+  if (api_->Init(NULL, language) == -1) {
+    throw std::runtime_error{"cannot initialize tesseract api"};
+  }
+}
+
+TesseractApi::TesseractApi(std::string_view language) : TesseractApi(language.data()) {}
+
+TesseractApi::~TesseractApi() { api_->End(); }
+
+auto LoadImage(const char *image_path) -> cv::Mat { return LoadImage(std::string_view(image_path)); }
+
+auto LoadImage(std::string_view image_path) -> cv::Mat {
+  if (image_path.empty()) {
+    throw std::invalid_argument("image path cannot be empty");
+  }
+
+  cv::Mat image = cv::imread(image_path.data(), cv::IMREAD_COLOR);
+  if (image.empty()) {
+    throw std::invalid_argument("image at path " + std::string(image_path) + "is empty");
+  }
+
+  return image;
+}
+
+auto Preprocess(const cv::Mat &image) noexcept -> cv::Mat { return image.clone(); }
+
+auto RecognizeText(const cv::Mat &image, float conf_threshold) noexcept
+    -> std::tuple<std::vector<std::string>, std::vector<cv::Rect>, std::vector<float>> {
+  if (image.empty()) {
+    return {};
+  }
+
+  auto tesseract = TesseractApi();
+
+  cv::Mat processed_image = Preprocess(image);
+
+  tesseract.api_->SetImage((uchar *)processed_image.data, processed_image.size().width, processed_image.size().height,
+                           processed_image.channels(), processed_image.step1());
+  tesseract.api_->Recognize(0);
+
+  std::vector<cv::Rect> boxes;
+  std::vector<std::string> texts;
+  std::vector<float> confs;
+
+  auto it = tesseract.api_->GetIterator();
+  const tesseract::PageIteratorLevel level = tesseract::RIL_WORD;
+
+  if (!it) {
+    return {};
+  }
+
+  do {
+    float conf = it->Confidence(level);
+
+    if (conf < conf_threshold) {
+      continue;
     }
+
+    confs.push_back(conf);
+
+    const char *word = it->GetUTF8Text(level);
+    texts.emplace_back(word);
+
+    int x1, y1, x2, y2;
+    it->BoundingBox(level, &x1, &y1, &x2, &y2);
+    boxes.emplace_back(cv::Point{x1, y1}, cv::Point{x2, y2});
+  } while (it->Next(level));
+
+  return {texts, boxes, confs};
+}
+
+auto MatchText(const cv::Mat &image, std::string_view target) noexcept -> cv::Point {
+  auto &[texts, boxes, confs] = RecognizeText(image, 70);
+
+  int min_distance = INT_MAX;
+  std::unique_ptr<const std::string> best_match;
+  cv::Rect match_box;
+
+  for (auto i = 0; i < texts.size(); ++i) {
+    const auto &text = texts[i];
+    const cv::Rect box = boxes[i];
+    float conf = confs[i];
+
+    std::cout << text << " " << conf << std::endl;
+
+    int distance = CalcLevenshteinDistance(text, target);
+    if (distance < min_distance && conf > 0.7) {
+      min_distance = distance;
+      match_box = box;
+      best_match = std::make_unique<const std::string>(text);
+      std::cout << "match set to " << *best_match << std::endl;
+    }
+  }
+
+  if (best_match == nullptr) {
+    return {};
+  }
+
+  return GetRectCenter(match_box);
+}
+
+auto MatchText(const cv::Mat &image, const char *target) noexcept -> cv::Point {
+  return MatchText(image, std::string_view(target));
+}
+
+auto MatchText(const char *image_path, const char *target) noexcept -> cv::Point {
+  cv::Mat image = LoadImage(image_path);
+  return MatchText(image, {target});
+}
+
+auto CalcLevenshteinDistance(std::string_view s1, std::string_view s2) noexcept -> int {
+  if (s1.empty()) {
+    return s2.size();
+  }
+
+  if (s2.empty()) {
+    return s1.size();
+  }
+
+  int l1 = s1.size();
+  int l2 = s2.size();
+
+  std::vector<std::vector<int>> matrix(l1 + 1, std::vector<int>(l2 + 1));
+
+  for (auto i = 0; i <= l1; ++i) {
+    matrix[i][0] = i;
+  }
+
+  for (auto j = 0; j <= l2; ++j) {
+    matrix[0][j] = j;
+  }
+
+  for (auto i = 1; i <= l1; ++i) {
+    for (auto j = 1; j <= l2; ++j) {
+      int cost = (s1[i - 1] == s2[j - 1]) ? 0 : 1;
+
+      matrix[i][j] = std::min({matrix[i - 1][j] + 1, matrix[i][j - 1] + 1, matrix[i - 1][j - 1] + cost});
+    }
+  }
+
+  return matrix[l1][l2];
+}
+
+auto GetRectCenter(const cv::Rect &rect) noexcept -> cv::Point {
+  return {rect.x + rect.width / 2, rect.y + rect.height / 2};
+}
+
+bool match_text_from_file(const char *image_path, const char *target, int *x, int *y) {
+  cv::Mat image = LoadImage(image_path);
+  return match_text(image, target, x, y);
+}
+
+bool match_text(const cv::Mat image, const char *target, int *x, int *y) {
+  auto pt = MatchText(image, target);
+  *x = pt.x;
+  *y = pt.y;
+  return true;
 }
