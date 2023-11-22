7c8520a287e6c03163293f4f7253ea3cf798d24e
Danxu Zhang <dxzhang49@gmail.com>
2023-11-22 15:52:39 +0800

    Update c binding interface

--- c_bindings/include/c_bind.hpp
+++ c_bindings/include/c_bind.hpp
@@ -1,39 +1,47 @@
 #pragma once
 
 #if defined(_WIN32) || defined(__CYGWIN__)
-    #define DLL_EXPORT __declspec(dllexport)
+#define DLL_EXPORT __declspec(dllexport)
 #else
-    #define DLL_EXPORT __attribute__((visibility("default")))
+#define DLL_EXPORT __attribute__((visibility("default")))
 #endif
 
 extern "C" {
 /**
- * @brief Matches text from a file and returns the coordinates of the match.
- * @param image_path Path to the image file containing the text.
- * @param target The text to search for within the image.
- * @param x Pointer to an integer to store the x-coordinate of the match.
- * @param y Pointer to an integer to store the y-coordinate of the match.
- * @return True if the text was successfully matched and coordinates set; otherwise, false.
+ * Detects and reads text from an image file.
+ *
+ * This function attempts to detect and read text from the image located at the specified path.
+ * The extracted texts are stored in an array, and the size of the array is updated to reflect
+ * the number of texts detected.
+ *
+ * @param image_path A pointer to a character string representing the path to the image file.
+ * @param texts A pointer to an array of character pointers, where the detected texts will be stored.
+ *              The array should be pre-allocated, and this function will fill in the text values.
+ * @param size A pointer to an integer that will be updated with the size of the texts array.
+ * @return Returns true if the text detection and reading are successful, false otherwise.
+ * @note It is the responsibility of the caller to allocate and free the memory for the texts array.
+ *       Ensure the image file exists and is accessible from the given path.
  */
-DLL_EXPORT bool match_text_from_file(const char *image_path, const char *target, int *x, int *y);
+DLL_EXPORT bool detect_read_text_from_file(const char *image_path, char **texts, int *size);
+
 /**
- * @brief Matches text within the given image data in memory and returns the coordinates of the match.
+ * Detects and reads text from an image in memory.
  *
- * This function searches for the specified target text within an image that is provided
- * as raw data in memory. The image data is expected to be in a specific format, defined by
- * the width, height, and type parameters. If the text is found, the function returns true
- * and sets the x and y coordinates to the starting point of the matched text within the image.
+ * This function attempts to detect and read text from an image data provided in memory.
+ * The image data should be in a specific format as defined by the type parameter. The extracted
+ * texts are stored in an array, and the size of the array is updated to reflect the number of
+ * texts detected.
  *
- * @param target The text to search for within the image data.
+ * @param data A pointer to the image data in memory.
  * @param height The height of the image in pixels.
  * @param width The width of the image in pixels.
- * @param type An integer indicating the image data type and format (e.g., grayscale, RGB).
- * @param data A pointer to the image data in memory. The format of this data must match
- *             the type specified.
- * @param x Pointer to an integer to store the x-coordinate of the match.
- * @param y Pointer to an integer to store the y-coordinate of the match.
- *
- * @return True if the text was successfully matched and coordinates set; otherwise, false.
+ * @param type An integer representing the image data type/format.
+ * @param texts A pointer to an array of character pointers, where the detected texts will be stored.
+ *              The array should be pre-allocated, and this function will fill in the text values.
+ * @param size A pointer to an integer that will be updated with the size of the texts array.
+ * @return Returns true if the text detection and reading are successful, false otherwise.
+ * @note The caller is responsible for ensuring that the image data is in the correct format and
+ *       for managing the memory for the texts array.
  */
-DLL_EXPORT bool match_text(const char *target, int height, int width, int type, void *data, int *x, int *y);
+DLL_EXPORT bool detect_read_text(void *data, int height, int width, int type, char **texts, int *size);
 }
--- c_bindings/src/c_bind.cpp
+++ c_bindings/src/c_bind.cpp
@@ -2,18 +2,26 @@
 #include "textspotter/textspotter.hpp"
 #include "textspotter/utility.hpp"
 
-bool match_text_from_file(const char *image_path, const char *target, int *x, int *y) {
-  cv::Mat image = LoadImage(image_path);
-  auto pt = MatchWord(image, target);
-  *x = pt.x;
-  *y = pt.y;
+auto detect_read_helper = [](const cv::Mat &image, char **texts, int *size) -> void {
+  const auto res = DetectReadTextMultiThread(image);
+  *size = res.size();
+
+  texts = (char **)malloc(res.size() * sizeof(char *));
+  for (size_t i = 0; i < res.size(); ++i) {
+    const auto text = res[i].text_;
+    texts[i] = (char *)malloc(text.size() * sizeof(char));
+    strcpy(texts[i], text.c_str());
+  }
+};
+
+DLL_EXPORT bool detect_read_text_from_file(const char *image_path, char **texts, int *size) {
+  const auto image = LoadImage(image_path);
+  detect_read_helper(image, texts, size);
   return true;
 }
 
-bool match_text(const char *text, int height, int width, int type, void *data, int *x, int *y) {
-  cv::Mat image{height, width, type, data};
-  auto pt = MatchWord(image, text);
-  *x = pt.x;
-  *y = pt.y;
+DLL_EXPORT bool detect_read_text(void *data, int height, int width, int type, char **texts, int *size) {
+  const cv::Mat image(height, width, type, data);
+  detect_read_helper(image, texts, size);
   return true;
 }
